02eb842799f1ce779b39b12c8da35eb6ef5e844c
milosz <milosz.p04@gmail.com>
2026-03-30 20:22:20 +0200

    add Rental class

--- RentalApp/Models/Equipment.cs
+++ RentalApp/Models/Equipment.cs
@@ -5,7 +5,6 @@ namespace RentalApp.Models;
 public abstract class Equipment(string name)
 {
     private static int _nextId = 1;
-    
     public int Id { get; } = _nextId++;
     public string Name { get; set; } = name;
     public RentalStatus Status { get; set; } = RentalStatus.Available;
---
+++ RentalApp/Models/Rental.cs
@@ -0,0 +1,26 @@
+namespace RentalApp.Models;
+
+public class Rental(User user, Equipment equipment, DateTime date, DateTime planedReturnDate)
+{
+    private static int _nextId = 1;
+
+    public int Id { get; } = _nextId++;
+    public User User { get; private set; } = user;
+    public Equipment Equipment { get; private set; } = equipment;
+    public DateTime Date { get; private set; } = date;
+    public DateTime PlannedReturnDate { get; private set; } = planedReturnDate;
+    
+    public DateTime?  ReturnDate { get; private set; }
+    public bool IsActive => ReturnDate == null;
+    public bool IsOverdue => IsActive && DateTime.Now < PlannedReturnDate;
+    
+    public void MarkAsReturned(DateTime returnDate)
+    {
+        if (!IsActive)
+        {
+            throw new InvalidOperationException("Ten sprzęt został już wcześniej zwrócony.");
+        }
+        
+        ReturnDate = returnDate;
+    }
+}
